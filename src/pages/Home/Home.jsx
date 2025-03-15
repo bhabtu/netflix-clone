6e330329e8c0855e521e8ff93bf2d5dc917c9d03
Blien Habtu <bhabtu622@gmail.com>
2025-03-14 20:27:05 -0400

    feat: Add hero section with banner, title, and description for sample show

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
+import hero_banner from "../../assets/hero-banner.jpeg";
+import hero_title from "../../assets/hero-title-r.png";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            When a young boy vanishes, a small town uncovers a mystery involving
+            secret experiments, terrifying supernatural forces and one strange
+            little girl.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
